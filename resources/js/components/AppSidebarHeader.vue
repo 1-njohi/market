518d3949953e4b0215869a42653af238746242bd
Wanjohi <kazibin66@gmail.com>
2026-09-13 16:55:52 +0300

    Update: Display balance

--- resources/js/components/AppSidebarHeader.vue
+++ resources/js/components/AppSidebarHeader.vue
@@ -114,6 +114,7 @@ watch(
                         <span
                             class="font-mono text-xs font-black text-emerald-400"
                         >
+                            KES
                             {{
                                 Number(
                                     $page.props.auth.balance || 0,
